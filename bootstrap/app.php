79259ace8580ddf200949143ee659e4e4e944f1b
ryantro <mryanrizki11@gmail.com>
2026-08-24 22:44:27 +0700

    chore: skrip ngrok share + trust proxy untuk preview & deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Percaya header proxy (ngrok / cPanel di belakang proxy) agar HTTPS & host terdeteksi benar.
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
